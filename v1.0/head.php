798e5ac0cb200e1bec0d34da6e0e3f85e8c24756
orlandogvargas <orlandogvargas@gmail.com>
2020-05-17 12:33:11 -0300

    primera version

--- v1.0/head.php
+++ v1.0/head.php
@@ -3,25 +3,26 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
+
 <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+
 <!-- Bootstrap Core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
 <!-- Animate CSS -->
 <link href="css/animate.css" rel="stylesheet">
+
 <!-- Magnific popup CSS -->
 <link href="css/magnific-popup.css" rel="stylesheet">
+
 <!-- Owl carousel CSS -->
 <link href="css/owl.carousel.min.css" rel="stylesheet">
+
 <!-- Font-awesome -->
 <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+
 <!-- Google font-->
 <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,300i,400,500,600,700,800,900" rel="stylesheet">
+
 <!-- Main CSS -->
 <link href="style.css" rel="stylesheet">
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
--- v1.0/res/scripts.php
+++
@@ -1,6 +0,0 @@
-<script src="js/jquery.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="mail/js/form-validator.min.js" type="text/javascript"></script>
-<script src="mail/js/contact-form-script.js" type="text/javascript"></script>
-<script src="js/script.js"></script>
-<!-- Mirrored from themecrazy.net/html/sigma/index-static.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 16 May 2020 15:10:47 GMT -->
