819d54ed275e801f12dff28084b240ccefedf554
orlandogvargas <orlandogvargas@gmail.com>
2020-05-16 23:47:04 -0300

    Primera version de Binder

---
+++ v1.0/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- navbar-->
+    <?php include ('res/navbar.html'); ?>
+    
+  </body>
+</html>
